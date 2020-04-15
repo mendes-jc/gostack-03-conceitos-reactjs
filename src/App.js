16b7d9315efc0ddfb6faf9626437fab963e14915
Júlio César <mendes_jc@outlook.com>
2020-04-14 23:21:04 -0300

    Here's it!

--- src/App.js
+++ src/App.js
@@ -1,28 +1,73 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from './services/api';
 
 import "./styles.css";
 
+const initialRepo = {
+  title: '',
+  url: '',
+  techs: '',
+}
+
 function App() {
+  const [repositories, setRepositories] = useState([]);
+  const [newRepo, setNewRepo] = useState(initialRepo);
+
+  useEffect(() => {
+    async function fetchRepositories() {
+      const { data } = await api.get('/repositories');
+      setRepositories(data);
+    }
+    fetchRepositories();
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const { title, url, techs } = newRepo;
+    const { data } = await api.post('/repositories', {
+      title,
+      url,
+      techs: techs.split(';')
+    })
+    setRepositories([...repositories, data]);
+    setNewRepo(initialRepo);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`/repositories/${id}`);
+    
+    setRepositories(repositories.filter(repo => repo.id !== id));
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(repository => 
+          <div key={repository.id}> 
+            <li>
+              {repository.title}
+              <button onClick={() => handleRemoveRepository(repository.id)}>
+                Remover
+              </button>
+            </li>
+          </div>
+        )}
       </ul>
-
+      Title
+      <input
+        value={newRepo.title}
+        onChange={event => setNewRepo({...newRepo, title: event.target.value })}
+      /><br/>
+      URL
+      <input
+        value={newRepo.url}
+        onChange={event => setNewRepo({...newRepo, url: event.target.value })}
+      /><br/>
+      Techs (separated by ';')
+      <input
+        value={newRepo.techs}
+        onChange={event => setNewRepo({...newRepo, techs: event.target.value})}
+      /><br/>
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
